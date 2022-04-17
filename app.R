d3b42919658eb9fa6eb1569be876fa3498699dcb
Noel Peterson <nmpeterson@users.noreply.github.com>
2022-04-17 18:07:32 -0500

    Refactored

--- app.R
+++ app.R
@@ -38,7 +38,7 @@ ui <- fluidPage(theme = shinytheme("yeti"),
       h2("Adjust the chart"),
       sliderInput("xmax",
                   label = "Maximum income to display",
-                  min = 0,
+                  min = 50000,
                   max = max_income,
                   value = max_income,
                   step = 50000)
@@ -54,43 +54,52 @@ ui <- fluidPage(theme = shinytheme("yeti"),
 # Define server logic required to draw income distribution
 server <- function(input, output) {
 
-    output$earningsPlot <- renderPlot({
-        df <- pums
+  df <- reactive({
+    # Apply filters to PUMS data
+    pums %>% filter(
+      if (input$state != "All 50 States") {State == input$state} else {TRUE},
+      if (input$occupation != "All Occupations") {Occupation == input$occupation} else {TRUE},
+      if (input$sex != "Either Sex") {Sex == input$sex} else {TRUE},
+      if (input$age != "All Ages") {Age == input$age} else {TRUE},
+      if (input$raceeth != "All Races/Ethnicities") {`Race/Ethnicity` == input$raceeth} else {TRUE}
+    )
+  })
 
-        # Apply filters
-        if (input$state != "All 50 States") {df <- filter(df, State == input$state)}
-        if (input$occupation != "All Occupations") {df <- filter(df, Occupation == input$occupation)}
-        if (input$sex != "Either Sex") {df <- filter(df, Sex == input$sex)}
-        if (input$age != "All Ages") {df <- filter(df, Age == input$age)}
-        if (input$raceeth != "All Races/Ethnicities") {df <- filter(df, `Race/Ethnicity` == input$raceeth)}
+  output$earningsPlot <- renderPlot({
+    # Log info about current plot
+    chart_spec <- paste(input$state, input$occupation, input$sex, input$age, input$raceeth, sep = " | ")
+    df_med_inc <- round(weighted.median(df()$Income, df()$PWGTP))
+    cat(file=stderr(), "Filters:", chart_spec, "\n")
+    cat(file=stderr(), "Max display:", input$xmax, "\n")
+    cat(file=stderr(), "Obs:", nrow(df()), "\n")
+    cat(file=stderr(), "Median:", df_med_inc, "\n")
 
-        df_med_inc <- round(weighted.median(df$Income, df$PWGTP))
+    # Generate the plot
+    p <- ggplot(df()) +
+      geom_density(aes(x = Income, weight = PWGTP), data = pums, alpha = 0.15, fill = "black", col = NA) +
+      geom_density(aes(x = Income, weight = PWGTP), alpha = 0.65, fill = "darkblue", col = NA) +
+      geom_vline(xintercept = df_med_inc, col = "darkblue") +
+      annotate(geom = "text", x = df_med_inc, y = Inf, hjust = -0.1, vjust = 2,
+               label = paste("Median:", dollar(df_med_inc)), col = "darkblue") +
+      scale_x_continuous(limits = c(0, input$xmax), labels = label_dollar()) +
+      theme_minimal() +
+      theme(axis.text.y = element_blank(),
+            panel.grid.major.y = element_blank(),
+            panel.grid.minor.y = element_blank()) +
+      labs(title = "Annual Earnings of Full-Time Workers",
+           subtitle = chart_spec,
+           caption = paste(
+             "Only includes workers who reported working at least 35 hours per week",
+             "and at least 48 weeks per year (including paid time off).\n",
+             "Income includes wages, salary, commissions, bonuses and tips.",
+             "Other income sources (including from self-employment) are excluded.\n",
+             "The distribution is an estimate based on", comma(nrow(df())),
+             "surveyed individuals, representing", comma(sum(df()$PWGTP)), "workers.")) +
+      xlab("Annual earnings (2020 dollars)") +
+      ylab("Proportion of full-time workers")
 
-        p <- ggplot(df) +
-          geom_density(aes(x = Income, weight = PWGTP), data = pums, alpha = 0.15, fill = "black", col = NA) +
-          geom_density(aes(x = Income, weight = PWGTP), alpha = 0.65, fill = "darkblue", col = NA) +
-          geom_vline(xintercept = df_med_inc, col = "darkblue") +
-          annotate(geom = "text", x = df_med_inc, y = Inf, hjust = -0.1, vjust = 2,
-                   label = paste("Median:", dollar(df_med_inc)), col = "darkblue") +
-          scale_x_continuous(limits = c(0, input$xmax), labels = label_dollar()) +
-          theme_minimal() +
-          theme(axis.text.y = element_blank(),
-                panel.grid.major.y = element_blank(),
-                panel.grid.minor.y = element_blank()) +
-          labs(title = "Annual Earnings of Full-Time Workers",
-               subtitle = paste(input$state, input$occupation, input$sex, input$age, input$raceeth, sep = " | "),
-               caption = paste(
-                 "Only includes workers who reported working at least 35 hours per week",
-                 "and at least 48 weeks per year (including paid time off).\n",
-                 "Income includes wages, salary, commissions, bonuses and tips.",
-                 "Other income sources (including from self-employment) are excluded.\n",
-                 "The distribution is an estimate based on", comma(nrow(df)),
-                 "surveyed individuals, representing", comma(sum(df$PWGTP)), "workers.")) +
-          xlab("Annual earnings (2020 dollars)") +
-          ylab("Proportion of full-time workers")
-
-        p
-    })
+    return(p)
+  })
 }
 
 # Run the application
